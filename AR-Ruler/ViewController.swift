bbb8f6d4c65947351be6e11d210fbd5a087ea742
Vickat <vickatyshev@gmail.com>
2020-04-10 00:26:57 -0400

    distance calculated. portrait mode only emforced

--- AR-Ruler/ViewController.swift
+++ AR-Ruler/ViewController.swift
@@ -14,16 +14,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     @IBOutlet var sceneView: ARSCNView!
     
+    var dotManyNodes =  [SCNNode]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
         sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
-        
-        
-        
-        
+       
+    
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -57,7 +57,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     func addDot(at hitResult : ARHitTestResult) {
-        let dotGeometry = SCNSphere(radius: 0.005)
+        let dotGeometry = SCNSphere(radius: 0.003)
         let material = SCNMaterial()
         material.diffuse.contents = UIColor.green
         dotGeometry.materials = [material]
@@ -66,7 +66,26 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         dotNode.position = SCNVector3(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y, hitResult.worldTransform.columns.3.z)
         sceneView.scene.rootNode.addChildNode(dotNode)
-        ///
+        
+        dotManyNodes.append(dotNode)
+        
+        if dotManyNodes.count >= 2 {
+            calculate()
+        }
     }
-    
+    func calculate() {
+        let start = dotManyNodes[0]
+        let end = dotManyNodes[1]
+      //  print(start.position)
+      //  print(end.position)
+        
+        let a = end.position.x - start.position.x
+        let b = end.position.y - start.position.y
+        let c = end.position.z - start.position.z
+        
+        let distance =  sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2))
+        
+        print(abs(distance))
+        
+    }
 }
